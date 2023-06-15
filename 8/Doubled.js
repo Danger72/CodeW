50e544612d1f07a028f8256caeff08cbccfec4c3
Dane Willett <dane.willett@gmail.com>
2023-06-14 21:18:59 -0700

    Doubled 8kyu codewars initial commit

---
+++ 8/Doubled.js
@@ -0,0 +1,17 @@
+//Given an array of integers, return a new array with each value doubled.
+
+//For example:
+
+//[1, 2, 3] --> [2, 4, 6]
+
+
+
+
+function doubled(arr){
+    return 
+    arr.map(element=>element*2)
+}
+
+
+doubled([1,2,3])
+console.log(doubled)([2,3,4]), [4,6,8]
